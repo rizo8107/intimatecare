0b70aba5b084bc05b83e03c4677282dfc5a573e1
Khushboo Bist <khushboobist@gmail.com>
2025-04-13 13:39:51 +0530

    Fix navbar overlapping hero section on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
       <BrowserRouter>
         <div className="flex flex-col min-h-screen">
           <Navbar />
-          <main className="flex-grow">
+          <main className="flex-grow pt-16 md:pt-20">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/about" element={<About />} />
